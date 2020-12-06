fa66518fc79b5c1130b182e700027cca59e751c3
tomu28 <tomsr2828@gmail.com>
2020-12-07 00:35:46 +0900

    Add: Entities

---
+++ CtaReactiveMaster/Sources/Entities/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  CtaReactiveMaster
+//
+//  Created by 小幡 十矛 on 2020/12/07.
+//
+
+public enum Category: String, Decodable {
+    case business = "business"
+    case entertainment = "entertainment"
+    case general = "general"
+    case health = "health"
+    case science = "science"
+    case sports = "sports"
+    case technology = "technology"
+}
---
+++ CtaReactiveMaster/Sources/Entities/Language.swift
@@ -0,0 +1,23 @@
+//
+//  Language.swift
+//  CtaReactiveMaster
+//
+//  Created by 小幡 十矛 on 2020/12/07.
+//
+
+public enum Language: String, Decodable {
+    case ar = "ar"
+    case de = "de"
+    case en = "en"
+    case es = "es"
+    case fr = "fr"
+    case he = "he"
+    case it = "it"
+    case nl = "nl"
+    case no = "no"
+    case pt = "pt"
+    case ru = "ru"
+    case se = "se"
+    case ud = "ud"
+    case zh = "zh"
+}

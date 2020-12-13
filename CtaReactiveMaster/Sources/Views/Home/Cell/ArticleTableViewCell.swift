1816415d9090b8fa07ebe1c0f9fcb92aae15b5f1
tomu28 <tomsr2828@gmail.com>
2020-12-13 23:31:16 +0900

    Clean: remove `func awakeFromNib`

--- CtaReactiveMaster/Sources/Views/Home/Cell/ArticleTableViewCell.swift
+++ CtaReactiveMaster/Sources/Views/Home/Cell/ArticleTableViewCell.swift
@@ -13,10 +13,6 @@ final class ArticleTableViewCell: UITableViewCell {
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var descriptionLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-    }
-    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }

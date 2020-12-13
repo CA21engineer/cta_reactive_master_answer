9a5e95eef31d89464b91dbfc38a5fcc98555f0ca
tomu28 <tomsr2828@gmail.com>
2020-12-13 21:48:45 +0900

    Clean: @availableを追加
    処理が流れるべきではないことを明示するのが追加の意図

--- CtaReactiveMaster/Sources/Views/Home/HomeViewController.swift
+++ CtaReactiveMaster/Sources/Views/Home/HomeViewController.swift
@@ -26,6 +26,7 @@ final class HomeViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

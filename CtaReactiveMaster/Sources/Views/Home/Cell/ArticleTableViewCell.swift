cfac1dbce03eab49080b3da81e5688ac75b4ef01
tomu28 <tomsr2828@gmail.com>
2020-12-18 01:43:45 +0900

    Clean: autoresizing→autoLayout

--- CtaReactiveMaster/Sources/Views/Home/Cell/ArticleTableViewCell.swift
+++ CtaReactiveMaster/Sources/Views/Home/Cell/ArticleTableViewCell.swift
@@ -9,17 +9,44 @@ import UIKit
 import Nuke
 
 final class ArticleTableViewCell: UITableViewCell {
-    @IBOutlet private weak var articleImageView: UIImageView!
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    
+    @IBOutlet private weak var articleImageView: UIImageView! {
+        didSet {
+            articleImageView.contentMode = .scaleAspectFill
+            articleImageView.layer.cornerRadius = 4
+        }
+    }
+
+    @IBOutlet private weak var mainStackView: UIStackView! {
+        didSet {
+            mainStackView.backgroundColor = .clear
+        }
+    }
+
+    @IBOutlet private weak var titleLabel: UILabel! {
+        didSet {
+            titleLabel.font = .systemFont(ofSize: 17, weight: .bold)
+        }
+    }
+
+    @IBOutlet private weak var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.font = .systemFont(ofSize: 14)
+            descriptionLabel.numberOfLines = 2
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .clear
+    }
+
     // Todo: articleImageViewのキャッシュ化
     func setup(article: NewsSource.Article) {
-        guard let imageUrl = article.urlToImage else {
-            articleImageView.image = UIImage(named: "default")
-            return
+        if let imageUrl = article.urlToImage {
+            articleImageView.image = UIImage(url: imageUrl)
+        } else {
+            articleImageView.image = #imageLiteral(resourceName: "default")
         }
-        articleImageView.image = UIImage(url: imageUrl)
         titleLabel.text = article.title
         descriptionLabel.text = article.description
     }
